a4d58783e8830f7408d50b7ae5fb76b0a40b735d
ARATA Mizuki <minorinoki@gmail.com>
2019-06-13 03:20:23 +0900

    Support unboxed vectors

--- src/Numeric/Rounded/Hardware/Internal.hs
+++ src/Numeric/Rounded/Hardware/Internal.hs
@@ -3,6 +3,8 @@
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
 module Numeric.Rounded.Hardware.Internal
   ( module Numeric.Rounded.Hardware.Internal
   , module Numeric.Rounded.Hardware.Rounding
@@ -17,6 +19,10 @@ import Data.Proxy
 import Data.Ratio
 import GHC.Generics (Generic)
 import Control.DeepSeq (NFData(..))
+import qualified Data.Vector.Unboxed as U
+import qualified Data.Vector.Unboxed.Mutable as UM
+import qualified Data.Vector.Generic as G
+import qualified Data.Vector.Generic.Mutable as GM
 
 foreign import ccall unsafe "rounded_hw_add_up"
   c_rounded_add_up :: Double -> Double -> Double
@@ -131,3 +137,32 @@ instance (RoundedPrim rn) => Fractional (RoundedDouble rn) where
   lhs@(RoundedDouble x) / RoundedDouble y = RoundedDouble (divDouble lhs x y)
 
 foreign import ccall unsafe "nextafter" c_nextafter :: Double -> Double -> Double
+
+newtype instance UM.MVector s (RoundedDouble rn) = MV_RoundedDouble (UM.MVector s Double)
+newtype instance U.Vector (RoundedDouble rn) = V_RoundedDouble (U.Vector Double)
+
+instance GM.MVector UM.MVector (RoundedDouble rn) where
+  basicLength (MV_RoundedDouble mv) = GM.basicLength mv
+  basicUnsafeSlice i l (MV_RoundedDouble mv) = MV_RoundedDouble (GM.basicUnsafeSlice i l mv)
+  basicOverlaps (MV_RoundedDouble mv) (MV_RoundedDouble mv') = GM.basicOverlaps mv mv'
+  basicUnsafeNew l = MV_RoundedDouble <$> GM.basicUnsafeNew l
+  basicInitialize (MV_RoundedDouble mv) = GM.basicInitialize mv
+  basicUnsafeReplicate i x = MV_RoundedDouble <$> GM.basicUnsafeReplicate i (coerce x)
+  basicUnsafeRead (MV_RoundedDouble mv) i = coerce <$> GM.basicUnsafeRead mv i
+  basicUnsafeWrite (MV_RoundedDouble mv) i x = GM.basicUnsafeWrite mv i (coerce x)
+  basicClear (MV_RoundedDouble mv) = GM.basicClear mv
+  basicSet (MV_RoundedDouble mv) x = GM.basicSet mv (coerce x)
+  basicUnsafeCopy (MV_RoundedDouble mv) (MV_RoundedDouble mv') = GM.basicUnsafeCopy mv mv'
+  basicUnsafeMove (MV_RoundedDouble mv) (MV_RoundedDouble mv') = GM.basicUnsafeMove mv mv'
+  basicUnsafeGrow (MV_RoundedDouble mv) n = MV_RoundedDouble <$> GM.basicUnsafeGrow mv n
+
+instance G.Vector U.Vector (RoundedDouble rn) where
+  basicUnsafeFreeze (MV_RoundedDouble mv) = V_RoundedDouble <$> G.basicUnsafeFreeze mv
+  basicUnsafeThaw (V_RoundedDouble v) = MV_RoundedDouble <$> G.basicUnsafeThaw v
+  basicLength (V_RoundedDouble v) = G.basicLength v
+  basicUnsafeSlice i l (V_RoundedDouble v) = V_RoundedDouble (G.basicUnsafeSlice i l v)
+  basicUnsafeIndexM (V_RoundedDouble v) i = coerce <$> G.basicUnsafeIndexM v i
+  basicUnsafeCopy (MV_RoundedDouble mv) (V_RoundedDouble v) = G.basicUnsafeCopy mv v
+  elemseq (V_RoundedDouble v) x y = G.elemseq v (coerce x) y
+
+instance U.Unbox (RoundedDouble rn)
