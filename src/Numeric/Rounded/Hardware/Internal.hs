1c2807756243cc2937671f3e3f297c8f4989ece1
ARATA Mizuki <minorinoki@gmail.com>
2019-12-28 16:02:56 +0900

    Add Internal module to export internal stuff

---
+++ src/Numeric/Rounded/Hardware/Internal.hs
@@ -0,0 +1,10 @@
+module Numeric.Rounded.Hardware.Internal
+  ( module Internal
+  ) where
+import           Numeric.Rounded.Hardware.Backend.Default    ()
+import           Numeric.Rounded.Hardware.Base.Class         as Internal
+import           Numeric.Rounded.Hardware.Base.Constants     as Internal
+import           Numeric.Rounded.Hardware.Base.Conversion    as Internal
+import           Numeric.Rounded.Hardware.Base.RoundedResult as Internal
+import           Numeric.Rounded.Hardware.Base.Rounding      as Internal
+import           Numeric.Rounded.Hardware.Base.Show          as Internal
