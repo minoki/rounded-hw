be8ec55e3003de79483376e8d2ccd89528f9fe84
ARATA Mizuki <minorinoki@gmail.com>
2020-01-04 03:20:34 +0900

    More constants

---
+++ etc/gen-constants-with-mpfr.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <gmp.h>
+#include <mpfr.h>
+
+int main(void)
+{
+    int precs[] = {53, 24};
+    for (int i = 0; i < sizeof(precs)/sizeof(precs[0]); ++i) {
+        int prec = precs[i];
+
+        mpfr_t x, one, half, minus_half;
+
+        mpfr_init2(x, prec);
+
+        mpfr_init2(one, prec);
+        mpfr_set_ui(one, 1, MPFR_RNDN);
+
+        mpfr_init2(half, prec);
+        mpfr_set_d(half, 0.5, MPFR_RNDN);
+
+        mpfr_init2(minus_half, prec);
+        mpfr_set_d(minus_half, -0.5, MPFR_RNDN);
+
+        printf("-- prec = %d bits\n", prec);
+
+        mpfr_const_pi(x, MPFR_RNDD);
+        mpfr_printf("  pi_down = Rounded %Ra\n", x);
+        mpfr_const_pi(x, MPFR_RNDU);
+        mpfr_printf("  pi_up   = Rounded %Ra\n", x);
+
+        puts("  -- log(2)");
+        mpfr_const_log2(x, MPFR_RNDD);
+        mpfr_printf("  log2_down = Rounded %Ra\n", x);
+        mpfr_const_log2(x, MPFR_RNDU);
+        mpfr_printf("  log2_up   = Rounded %Ra\n", x);
+
+        puts("  -- exp(1)");
+        mpfr_exp(x, one, MPFR_RNDD);
+        mpfr_printf("  exp1_down = Rounded %Ra\n", x);
+        mpfr_exp(x, one, MPFR_RNDU);
+        mpfr_printf("  exp1_up   = Rounded %Ra\n", x);
+
+        puts("  -- exp(1/2)");
+        mpfr_exp(x, half, MPFR_RNDD);
+        mpfr_printf("  exp1_2_down = Rounded %Ra\n", x);
+        mpfr_exp(x, half, MPFR_RNDU);
+        mpfr_printf("  exp1_2_up   = Rounded %Ra\n", x);
+
+        puts("  -- exp(-1/2)");
+        mpfr_exp(x, minus_half, MPFR_RNDD);
+        mpfr_printf("  expm1_2_down = Rounded %Ra\n", x);
+        mpfr_exp(x, minus_half, MPFR_RNDU);
+        mpfr_printf("  expm1_2_up   = Rounded %Ra\n", x);
+
+        puts("  -- sqrt(2)");
+        mpfr_sqrt_ui(x, 2, MPFR_RNDD);
+        mpfr_printf("  sqrt2_down = Rounded %Ra\n", x);
+        mpfr_sqrt_ui(x, 2, MPFR_RNDU);
+        mpfr_printf("  sqrt2_up   = Rounded %Ra\n", x);
+
+        puts("  -- sqrt(2)-1");
+        mpfr_set_prec(x, prec * 2);
+        mpfr_sqrt_ui(x, 2, MPFR_RNDD);
+        mpfr_sub_ui(x, x, 1, MPFR_RNDD);
+        mpfr_prec_round(x, prec, MPFR_RNDD);
+        mpfr_printf("  sqrt2m1_down = Rounded %Ra\n", x);
+        mpfr_set_prec(x, prec * 2);
+        mpfr_sqrt_ui(x, 2, MPFR_RNDU);
+        mpfr_sub_ui(x, x, 1, MPFR_RNDU);
+        mpfr_prec_round(x, prec, MPFR_RNDU);
+        mpfr_printf("  sqrt2m1_up   = Rounded %Ra\n", x);
+
+        mpfr_clear(x);
+        mpfr_clear(one);
+        mpfr_clear(half);
+        mpfr_clear(minus_half);
+    }
+}
--- src/Numeric/Rounded/Hardware/Internal/Constants.hs
+++ src/Numeric/Rounded/Hardware/Internal/Constants.hs
@@ -1,31 +1,78 @@
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE HexFloatLiterals #-}
 module Numeric.Rounded.Hardware.Internal.Constants where
+import Numeric.Rounded.Hardware.Internal.Rounding
 
 class RealFloatConstants a where
   positiveInfinity :: a
   negativeInfinity :: a
   maxFinite :: a
   -- minPositiveNormal :: a
-  -- minPositiveSubnormal :: a
-  -- pi_up, pi_down
+  minPositive :: a
+  pi_down, log2_down, exp1_down, exp1_2_down, expm1_2_down, sqrt2_down, sqrt2m1_down :: Rounded 'TowardNegInf a
+  pi_up, log2_up, exp1_up, exp1_2_up, expm1_2_up, sqrt2_up, sqrt2m1_up :: Rounded 'TowardInf a
+  -- log(2), exp(1), exp(1/2), exp(-1/2), sqrt(2), sqrt(2)-1
 
 instance RealFloatConstants Double where
   positiveInfinity = 1/0
   negativeInfinity = -1/0
   maxFinite = 0x1.fffffffffffffp+1023
   -- minPositiveNormal = 0x1p-1022
-  -- minPositiveSubnormal = 0x1p-1074
+  minPositive = 0x1p-1074 -- subnormal
   -- pi_up      = 0x1.921fb54442d19p1
   -- pi_down    = 0x1.921fb54442d18p1
   -- (pi :: Double) == 0x1.921fb54442d18p1
+  pi_down = Rounded 0x3.243f6a8885a3p+0
+  pi_up   = Rounded 0x3.243f6a8885a32p+0
+  -- log(2)
+  log2_down = Rounded 0xb.17217f7d1cf78p-4
+  log2_up   = Rounded 0xb.17217f7d1cf8p-4
+  -- exp(1)
+  exp1_down = Rounded 0x2.b7e151628aed2p+0
+  exp1_up   = Rounded 0x2.b7e151628aed4p+0
+  -- exp(1/2)
+  exp1_2_down = Rounded 0x1.a61298e1e069bp+0
+  exp1_2_up   = Rounded 0x1.a61298e1e069cp+0
+  -- exp(-1/2)
+  expm1_2_down = Rounded 0x9.b4597e37cb048p-4
+  expm1_2_up   = Rounded 0x9.b4597e37cb05p-4
+  -- sqrt(2)
+  sqrt2_down = Rounded 0x1.6a09e667f3bccp+0
+  sqrt2_up   = Rounded 0x1.6a09e667f3bcdp+0
+  -- sqrt(2)-1
+  sqrt2m1_down = Rounded 0x6.a09e667f3bcc8p-4
+  sqrt2m1_up   = Rounded 0x6.a09e667f3bcccp-4
   {-# INLINE positiveInfinity #-}
   {-# INLINE negativeInfinity #-}
   {-# INLINE maxFinite #-}
+  {-# INLINE minPositive #-}
 
 instance RealFloatConstants Float where
   positiveInfinity = 1/0
   negativeInfinity = -1/0
   maxFinite = 0x1.fffffep+127
+  minPositive = 0x1p-149
+  pi_down = Rounded 0x3.243f68p+0
+  pi_up   = Rounded 0x3.243f6cp+0
+  -- log(2)
+  log2_down = Rounded 0xb.17217p-4
+  log2_up   = Rounded 0xb.17218p-4
+  -- exp(1)
+  exp1_down = Rounded 0x2.b7e15p+0
+  exp1_up   = Rounded 0x2.b7e154p+0
+  -- exp(1/2)
+  exp1_2_down = Rounded 0x1.a61298p+0
+  exp1_2_up   = Rounded 0x1.a6129ap+0
+  -- exp(-1/2)
+  expm1_2_down = Rounded 0x9.b4597p-4
+  expm1_2_up   = Rounded 0x9.b4598p-4
+  -- sqrt(2)
+  sqrt2_down = Rounded 0x1.6a09e6p+0
+  sqrt2_up   = Rounded 0x1.6a09e8p+0
+  -- sqrt(2)-1
+  sqrt2m1_down = Rounded 0x6.a09e6p-4
+  sqrt2m1_up   = Rounded 0x6.a09e68p-4
   {-# INLINE positiveInfinity #-}
   {-# INLINE negativeInfinity #-}
   {-# INLINE maxFinite #-}
+  {-# INLINE minPositive #-}
