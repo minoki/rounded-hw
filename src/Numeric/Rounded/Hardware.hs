fbf8a85d45bec946be9468a9b1dc9be855ed3e8d
ARATA Mizuki <minorinoki@gmail.com>
2020-01-25 11:24:07 +0900

    Export type classes from Numeric.Rounded.Hardware

--- src/Numeric/Rounded/Hardware.hs
+++ src/Numeric/Rounded/Hardware.hs
@@ -2,6 +2,10 @@ module Numeric.Rounded.Hardware
   ( Rounded(..)
   , RoundingMode(..)
   , getRounded
+  , Rounding
+  , RoundedRing
+  , RoundedFractional
+  , RoundedSqrt
   )
 where
 import           Numeric.Rounded.Hardware.Internal
