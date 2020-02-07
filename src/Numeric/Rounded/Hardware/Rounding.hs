4a4701dbbc077a2812938b02c674be23c4880785
ARATA Mizuki <minorinoki@gmail.com>
2020-02-08 00:34:26 +0900

    Add Numeric.Rounded.Hardware.Rounding module

---
+++ src/Numeric/Rounded/Hardware/Rounding.hs
@@ -0,0 +1,8 @@
+module Numeric.Rounded.Hardware.Rounding
+  ( RoundingMode(..)
+  , oppositeRoundingMode
+  , Rounding
+  , rounding
+  , reifyRounding
+  ) where
+import Numeric.Rounded.Hardware.Internal
