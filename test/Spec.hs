96d4a4f6498aca91eeb953b4fbc348072e28c8b5
ARATA Mizuki <minorinoki@gmail.com>
2020-02-09 08:37:28 +0900

    Print backend names in the test

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,23 +1,36 @@
 {-# LANGUAGE CPP #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+import           Data.Proxy
 import qualified FromIntegerSpec
 import qualified FromRationalSpec
 import qualified IntervalArithmeticSpec
+import           Numeric.Rounded.Hardware.Backend (backendName)
 import qualified RoundedArithmeticSpec
 import qualified ShowFloatSpec
 import           Test.Hspec
 #ifdef TEST_X87_LONG_DOUBLE
+import           Numeric.LongDouble (LongDouble)
 import qualified X87LongDoubleSpec
 #endif
 
-main :: IO ()
-main = hspec $ do
-  describe "fromInteger" FromIntegerSpec.spec
-  describe "fromRational" FromRationalSpec.spec
-  describe "showFloat" ShowFloatSpec.spec
-  describe "rounded arithmetic" RoundedArithmeticSpec.spec
-  describe "interval arithmetic" IntervalArithmeticSpec.spec
+printBackends :: IO ()
+printBackends = do
+  putStrLn $ "Backend for Double: " ++ backendName (Proxy :: Proxy Double)
+  putStrLn $ "Backend for Float: " ++ backendName (Proxy :: Proxy Float)
 #ifdef TEST_X87_LONG_DOUBLE
-  describe "x87 long double" X87LongDoubleSpec.spec
+  putStrLn $ "Backend for LongDouble: " ++ backendName (Proxy :: Proxy LongDouble)
+#endif
+
+main :: IO ()
+main = do
+  printBackends
+  hspec $ do
+    describe "fromInteger" FromIntegerSpec.spec
+    describe "fromRational" FromRationalSpec.spec
+    describe "showFloat" ShowFloatSpec.spec
+    describe "rounded arithmetic" RoundedArithmeticSpec.spec
+    describe "interval arithmetic" IntervalArithmeticSpec.spec
+#ifdef TEST_X87_LONG_DOUBLE
+    describe "x87 long double" X87LongDoubleSpec.spec
 #endif
