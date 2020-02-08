b72f06fd72d4f13b6d1ba3d1f6d11a6a2963ea75
ARATA Mizuki <minorinoki@gmail.com>
2020-02-08 23:24:23 +0000

    Update test for x87 long double

--- test/X87LongDoubleSpec.hs
+++ test/X87LongDoubleSpec.hs
@@ -1,28 +1,28 @@
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE ScopedTypeVariables #-}
 {-# OPTIONS_GHC -Wno-orphans #-}
 module X87LongDoubleSpec where
 import           Data.Proxy
 import qualified FromIntegerSpec
 import qualified FromRationalSpec
+import qualified IntervalArithmeticSpec
 import           Numeric.LongDouble (LongDouble)
-import           Numeric.Rounded.Hardware.Backend.X87LongDouble ()
 import qualified RoundedArithmeticSpec
 import qualified ShowFloatSpec
 import           Test.Hspec
 import           Test.QuickCheck.Arbitrary
 
+-- orphan instance
 instance Arbitrary LongDouble where
   arbitrary = arbitrarySizedFractional
   shrink = shrinkDecimal
 
 spec :: Spec
 spec = do
-  let ldProxy :: Proxy LongDouble
-      ldProxy = Proxy
-  describe "rounded arithmetic" $ RoundedArithmeticSpec.specT ldProxy
-  describe "rounded arithmetic" $ RoundedArithmeticSpec.verifyImplementation ldProxy ldProxy
-  describe "fromInteger"        $ FromIntegerSpec.specT ldProxy
-  describe "fromRational"       $ FromRationalSpec.specT ldProxy
-  describe "showFloat"          $ ShowFloatSpec.specT ldProxy
+  describe "rounded arithmetic"  $ RoundedArithmeticSpec.specT ldProxy
+  describe "rounded arithmetic"  $ RoundedArithmeticSpec.verifyImplementation ldProxy ldProxy
+  describe "interval arithmetic" $ IntervalArithmeticSpec.verifyImplementation ldProxy
+  describe "fromInteger"         $ FromIntegerSpec.specT ldProxy
+  describe "fromRational"        $ FromRationalSpec.specT ldProxy
+  describe "showFloat"           $ ShowFloatSpec.specT ldProxy
+  where
+    ldProxy :: Proxy LongDouble
+    ldProxy = Proxy
